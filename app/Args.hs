1b4c2fdc3a8cf0fc6494486efc8d37c0d1b50a09
m3dry <m3dry@tutamail.com>
2023-06-30 16:38:27 +0200

    feat: actuall cli args parsing

--- app/Args.hs
+++ app/Args.hs
@@ -1,162 +1,59 @@
-module Args where
+module Args (args, Command, Day, Args) where
 
-import Args.Doc qualified as Doc
-import Args.Parser
-import Control.Monad.State
-import Data.Maybe
-import Debug.Trace
+import Options.Applicative
 
 data Command
-  = New {editor :: Bool}
-  | Show
-  | Edit
-  deriving (Show)
-
-commandP =
-  modesDefault
-    [ ( "new",
-        let docTemplate doc =
-              ( Doc.CommandInfo "new" "Create a new heffal file",
-                Doc.SubCommand
-                  { Doc.subCmdUsage =
-                      Doc.Usage
-                        { Doc.options = [doc],
-                          Doc.enums = [],
-                          Doc.commands = []
-                        }
-                  }
-              )
-         in do
-              args <- get
-              f <- flagBool "Open file in an editor" ["-e", "--edit"]
-              return $ case f of
-                A e docF -> A (New e) $ docTemplate docF
-                ADoc docF -> ADoc $ docTemplate docF
-      ),
-      ( "show",
-        showP
-      ),
-      ( "edit",
-        let docTemplate =
-              ( Doc.CommandInfo "edit" "Edit a heffal file",
-                Doc.SubCommand
-                  { Doc.subCmdUsage =
-                      Doc.Usage
-                        { Doc.options = [],
-                          Doc.enums = [],
-                          Doc.commands = []
-                        }
-                  }
-              )
-         in do
-              a <- get
-              return $ case a of
-                SDoc -> ADoc docTemplate
-                _ -> A Edit docTemplate
-      )
-    ] showP
-  where
-    showP =
-      let docTemplate =
-            ( Doc.CommandInfo "show" "Render a heffal file",
-              Doc.SubCommand
-                { Doc.subCmdUsage =
-                    Doc.Usage
-                      { Doc.options = [],
-                        Doc.enums = [],
-                        Doc.commands = []
-                      }
-                }
-            )
-       in do
-            a <- get
-            return $ case a of
-              SDoc -> ADoc docTemplate
-              _ -> A Show docTemplate
+    = Show
+    | Edit
+    | New Bool
+    deriving (Show)
 
 data Day
-  = Today
-  | Yesterday
-  | Tomorrow
-  deriving (Show)
+    = Today
+    | Tomorrow
+    | Yesterday
+    deriving (Show)
 
-dayP = enumDefault "DAY" "from which day" [("y", Yesterday), ("t", Today), ("tmr", Tomorrow)] Today
+instance Read Day where
+    readsPrec _ "t" = [(Today, "")]
+    readsPrec _ "tmr" = [(Tomorrow, "")]
+    readsPrec _ "y" = [(Yesterday, "")]
+    readsPrec _ _ = []
 
 data Args
-  = Todo {day :: Day, command :: Command}
-  | File {fName :: String, command :: Command}
-  deriving (Show)
+    = Todo Day Command
+    | File String Command
+    deriving (Show)
 
-todoP =
-  let docTemplate day cmd =
-        ( Doc.CommandInfo
-            { Doc.cName = "todo",
-              Doc.cDesc = "Todo actions"
-            },
-          Doc.SubCommand
-            { Doc.subCmdUsage =
-                Doc.Usage
-                  { Doc.options = [],
-                    Doc.enums = [day],
-                    Doc.commands = cmd
-                  }
-            }
+commandP :: Parser Command
+commandP =
+    subparser
+        ( command "show" (info (pure Show) (progDesc "Print out the formatted contents of a file"))
+       <> command "edit" (info (pure Edit) (progDesc "Open a file in an editor"))
+       <> command
+            "new"
+            ( info
+                ((New <$> switch (long "edit" <> short 'e' <> help "Open the file in an editor")) <**> helper)
+                (progDesc "Create a new file")
+            )
         )
-   in do
-        d <- dayP
-        c <- commandP
-        let (day, dDoc) = case d of
-                            A day doc -> (Just day, docTemplate doc)
-                            ADoc doc -> (Nothing, docTemplate doc)
-        let (cmd, doc) = case c of
-                            A cmd doc -> (Just cmd, dDoc doc)
-                            ADoc doc -> (Nothing, dDoc doc)
-        return $ case (day, cmd) of
-            (Just d, Just c) -> A (Todo d c) doc
-            _ -> ADoc doc
 
-fileP =
-  let docTemplate doc =
-        ( Doc.CommandInfo {Doc.cName = "-f", Doc.cDesc = "File actions"},
-          Doc.SubCommand
-            { Doc.subCmdUsage =
-                Doc.Usage
-                  { Doc.options = [],
-                    Doc.enums = [],
-                    Doc.commands = doc
-                  }
-            }
-        )
-   in do
-        a <- get
-        name <- case a of
-          SDoc -> return Nothing
-          SArgs args -> do
-            Just <$> pop
-        cmd <- commandP
-        let (r, doc) = case cmd of
-              ADoc doc -> (Nothing, doc)
-              A r doc -> (Just r, doc)
-        return $ case (name, r) of
-          (Just name, Just cmd) -> A (File name cmd) $ docTemplate doc
-          _ -> ADoc $ docTemplate doc
+fileP :: Parser Args
+fileP = File <$> strArgument (metavar "FILE" <> action "file") <*> commandP
 
+todoP :: Parser Args
+todoP = Todo <$> argument auto (metavar "DAY" <> help "Which todo file [t,tmr,y]" <> completeWith ["t", "tmr", "y"]) <*> commandP
+
+argsP :: Parser Args
 argsP =
-  let docTemplate doc =
-        Doc.Doc
-          { Doc.dName = "Heffal",
-            Doc.version = "0.1.0",
-            Doc.summary = "Heffal client",
-            Doc.usage =
-              Doc.Usage
-                { Doc.options = [],
-                  Doc.enums = [],
-                  Doc.commands = doc
-                }
-          }
-   in do
-        m <- modesDefault [("-f", fileP), ("todo", todoP)] todoP
-        return $ case m of
-          A r cmd -> A r $ docTemplate cmd
-          ADoc cmd -> AFailed $ const (Doc.help $ docTemplate cmd)
-          AFailed str -> AFailed str
+    subparser
+        ( command
+            "todo"
+            (info (todoP <**> helper) (progDesc "Perform an action on a todo file"))
+       <> command
+           "file"
+           (info (fileP <**> helper) (progDesc "Perform an action on a file"))
+        )
+
+args :: IO Args
+args = execParser $ info (argsP <**> helper) (fullDesc <> header "Heffal - a cli client for heffal")
--- app/Main.hs
+++ app/Main.hs
@@ -1,22 +1,13 @@
--- {-# LANGUAGE QuasiQuotes #-}
-
 module Main where
 
--- import Heffal.Config
--- import Heffal.Format
--- import Heffal.Format.Cli
--- import Heffal.Format.Eww
--- import Heffal.Format.Json
--- import Heffal.Lexer
--- import Heffal.Parser
-
-import Args
-import Args.Doc (OptionValue (OptionValue))
-import Args.Parser
 import Control.Monad.State
 import Control.Monad.Trans.Maybe
 import Data.Maybe
 
+import Args
+
 main :: IO ()
-main = do
-    print $ runArgs argsP $ SArgs ["-t", "show"]
+main = main' =<< args
+
+main' :: Args -> IO ()
+main' = print
